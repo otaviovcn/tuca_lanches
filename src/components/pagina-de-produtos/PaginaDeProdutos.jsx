b8022aeb1d8c6f2a6f4511082d79b3de00161cc3
Otavio Vinicius <viinident@gmail.com>
2023-06-16 19:41:23 -0300

    :lipstick: 	feat: Estilização de interface da página de cadastro de produtos

--- src/components/pagina-de-produtos/PaginaDeProdutos.jsx
+++ src/components/pagina-de-produtos/PaginaDeProdutos.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Grid, Container, Button } from '@mui/material';
+import { Grid, Container, Button, Box } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
 import { CadastroDeProdutos } from './CadastroDeProdutos';
@@ -12,20 +12,21 @@ export const PaginaDeProdutos = () => {
 
   const containersList = [
     <CadastroDeProdutos />,
-    <ProdutosCadastrados productType="Comida" />,
-    <ProdutosCadastrados productType="Bebida" />,
+    <ProdutosCadastrados />,
   ]
 
   return (
     <Container sx={{ marginTop: theme.spacing(5), width: theme.spacing(100), maxHeight: 80 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {containersList.map((item, index) => (
-          <Grid max item xs={20} sm={8} md={18} key={index}>
-            {item}
-          </Grid>
-        ))}
-      </Grid>
-          <Button sx={{ background: "red"}} variant="contained" onClick={() => removeLocalStorage('tuca_lanches_produtos')}>Limpar Produtos</Button>
+      {/* <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}> */}
+      {containersList.map((item, index) => (
+        // <Grid max item xs={20} sm={8} md={18} key={index}>
+        <Box sx={{ width: '100%', background: 'white', marginTop: 2, marginBottom: 2 }}>
+          {item}
+        </ Box>
+        // </Grid>
+      ))}
+      {/* </Grid> */}
+      <Button sx={{ background: "red", display: "none" }} variant="contained" onClick={() => removeLocalStorage('tuca_lanches_produtos')}>Limpar Produtos</Button>
     </Container>
   )
 };
--- src/components/pagina-de-produtos/ProdutosCadastrados.jsx
+++ src/components/pagina-de-produtos/ProdutosCadastrados.jsx
@@ -1,16 +1,19 @@
-import { Button, CardContent, Grid, Box, Typography, Card, Avatar } from '@mui/material'
+import { Button, CardContent, Grid, Box, Typography, Card, Avatar, Tabs, Tab } from '@mui/material'
 import { useTheme } from '@mui/material/styles';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
 import EditIcon from '@mui/icons-material/Edit'
+import LunchDiningIcon from '@mui/icons-material/LunchDining';
+import LocalBarIcon from '@mui/icons-material/LocalBar';
 // import { Avatar } from '@mui/material';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useProdutosContext } from '../../contexts/ProdutosContext';
 import { setLocalStorage } from '../../utils/localStorage';
 
 
-export const ProdutosCadastrados = ({ productType }) => {
+export const ProdutosCadastrados = () => {
+  const [value, setValue] = useState(0);
   const theme = useTheme();
   const { produtos, setProdutosContext, setProductIsUpdatingContext } = useProdutosContext();
 
@@ -23,50 +26,117 @@ export const ProdutosCadastrados = ({ productType }) => {
     setProdutosContext(newProductsList);
   };
 
-  return (
-    <Card>
-      <CardContent>
-        <Grid container spacing={1}>
-          <Typography gutterBottom variant="h6" align="center" sx={{ color: theme.palette.secondary.main, fontSize: 19 }}>
-            {`${productType}s cadastradas`}
-          </Typography>
-          <Grid item xs={12} sm={12} sx={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
-            {Object.values(produtos).map(({ id, name, costPrice, productPrice, type, imgLink }) => {
-              if (type !== productType) return null;
-              return (
-                <Box sx={
-                  { display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "column", border: 1, borderColor: theme.palette.secondary.main, borderRadius: 1, padding: 1, margin: 1, backgroundColor: theme.palette.background.default, width: "130px" }
-                }>
-                  <Avatar sx={{ width: 80, height: 80, alignSelf: "center" }} alt={name} src={imgLink} />
-                  <Typography key={id} gutterBottom variant="h6" align="center" sx={{ color: theme.palette.secondary.dark, fontSize: 17 }}>
-                    {name}
-                  </Typography>
-                  <Box sx={
-                    { display: "flex", justifyContent: "center" }
-                  }>
-                    <Typography key={id} gutterBottom variant="h6" align="center" sx={{ color: theme.palette.secondary.light, fontSize: 15, paddingLeft: 1, paddingRight: 1 }}>
-                      {`Custo: R$${costPrice}`}
-                    </Typography>
-                    <Typography key={id} gutterBottom variant="h6" align="center" sx={{ color: theme.palette.secondary.light, fontSize: 15, paddingLeft: 1, paddingRight: 1 }}>
-                      {`Valor: RS${productPrice}`}
-                    </Typography>
-                  </Box>
-                  <Box sx={{ display: "flex" }}>
-                    <Button variant="text" onClick={() => deleteUser(id)}><DeleteForeverIcon /></Button>
-                    <Button
-                      variant="text"
-                      onClick={() => setProductIsUpdatingContext(true, id)}
-                    >
-                      <EditIcon />
-                    </Button>
+  const productsType = ["Comida", "Bebida"]
 
-                  </Box>
-                </Box>);
-            }
-            )}
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          {
+            productsType.map((type) => {
+              if (type === "Comida") {
+                return <Tab icon={<LunchDiningIcon />} label={`${type}s`} />
+              }
+              return <Tab icon={<LocalBarIcon />} label={`${type}s`} />
+            })
+          }
+        </Tabs>
+      </Box>
+
+      <Box sx={{ display: "flex", flexWrap: "wrap" }} value={value} index={0}>
+        {
+          Object.values(produtos).map(({ id, productPrice, costPrice, type, name, imgLink, category }) => {
+            return (
+              <>
+                {productsType[value] === type && (
+                  <>
+                    <Box sx={{ minWidth: 115, margin: 1 }}>
+                      <Card variant="outlined" sx={{ width: 110 }}>
+                        <CardContent>
+                          <Avatar sx={{ width: 80, height: 80, alignSelf: "center" }} alt={name} src={imgLink} />
+                          <Typography sx={{ fontSize: 14, textAlign: "center" }} color={theme.palette.secondary.dark}>
+                            {name}
+                          </Typography>
+                        </CardContent>
+                        <Box sx={
+                          { display: "flex", justifyContent: "center" }
+                        }>
+                          <Typography key={id} gutterBottom variant="h6" align="center" sx={{ color: theme.palette.secondary.light, fontSize: 15, paddingLeft: 1, paddingRight: 1 }}>
+                            {`Custo: R$${costPrice}`}
+                          </Typography>
+                          <Typography key={id} gutterBottom variant="h6" align="center" sx={{ color: theme.palette.secondary.light, fontSize: 15, paddingLeft: 1, paddingRight: 1 }}>
+                            {`Valor: RS${productPrice}`}
+                          </Typography>
+                        </Box>
+                        <Box sx={{ display: "flex" }}>
+                          <Button variant="text" onClick={() => deleteUser(id)}><DeleteForeverIcon /></Button>
+                          <Button
+                            variant="text"
+                            onClick={() => setProductIsUpdatingContext(true, id)}
+                          >
+                            <EditIcon />
+                          </Button>
+
+                        </Box>
+                      </Card>
+                    </Box >
+                  </>
+                )}
+              </>
+            );
+          })
+        }
+      </Box>
+
+
+      {/* <Card>
+        <CardContent>
+          <Grid container spacing={1}>
+            <Typography gutterBottom variant="h6" align="center" sx={{ color: theme.palette.secondary.main, fontSize: 19 }}>
+              {`${productType}s cadastradas`}
+            </Typography>
+            <Grid item xs={12} sm={12} sx={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+              {Object.values(produtos).map(({ id, name, costPrice, productPrice, type, imgLink }) => {
+                if (type !== productType) return null;
+                return (
+                  <Box sx={
+                    { display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "column", border: 1, borderColor: theme.palette.secondary.main, borderRadius: 1, padding: 1, margin: 1, backgroundColor: theme.palette.background.default, width: "130px" }
+                  }>
+                    <Avatar sx={{ width: 80, height: 80, alignSelf: "center" }} alt={name} src={imgLink} />
+                    <Typography key={id} gutterBottom variant="h6" align="center" sx={{ color: theme.palette.secondary.dark, fontSize: 17 }}>
+                      {name}
+                    </Typography>
+                    <Box sx={
+                      { display: "flex", justifyContent: "center" }
+                    }>
+                      <Typography key={id} gutterBottom variant="h6" align="center" sx={{ color: theme.palette.secondary.light, fontSize: 15, paddingLeft: 1, paddingRight: 1 }}>
+                        {`Custo: R$${costPrice}`}
+                      </Typography>
+                      <Typography key={id} gutterBottom variant="h6" align="center" sx={{ color: theme.palette.secondary.light, fontSize: 15, paddingLeft: 1, paddingRight: 1 }}>
+                        {`Valor: RS${productPrice}`}
+                      </Typography>
+                    </Box>
+                    <Box sx={{ display: "flex" }}>
+                      <Button variant="text" onClick={() => deleteUser(id)}><DeleteForeverIcon /></Button>
+                      <Button
+                        variant="text"
+                        onClick={() => setProductIsUpdatingContext(true, id)}
+                      >
+                        <EditIcon />
+                      </Button>
+
+                    </Box>
+                  </Box>);
+              }
+              )}
+            </Grid>
           </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card> */}
+    </>
   )
 }
