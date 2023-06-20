3d2752468f54924767221df7b023855ef0564b90
Otavio Vinicius <viinident@gmail.com>
2023-06-20 20:11:12 -0300

    dizzy: Coloca dinamismo no ícone de carrinho de compras

--- src/components/venda-do-produto/VendaDoProduto.jsx
+++ src/components/venda-do-produto/VendaDoProduto.jsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from 'react';
-import { Typography, Tab, Tabs, Box, Button, SpeedDialIcon, SpeedDial } from '@mui/material';
+import { Typography, Tab, Tabs, Box, Button, Badge, useTheme } from '@mui/material';
 import ProductionQuantityLimitsIcon from '@mui/icons-material/ProductionQuantityLimits';
 
 import { useProdutosContext } from '../../contexts/ProdutosContext';
@@ -7,56 +7,12 @@ import { CardProduct } from './CardProduct';
 import { setLocalStorage, getLocalStorage, removeLocalStorage } from '../../utils/localStorage';
 import { useCarrinhoContext } from '../../contexts/CarrinhoContext';
 import { useRelatoriosContext } from '../../contexts/RelatoriosContext';
-import { actualData, actualHour, adicionaAoRelatorio } from '../../utils/relatorios';
+import { adicionaAoRelatorio } from '../../utils/relatorios';
 
 import LunchDiningIcon from '@mui/icons-material/LunchDining';
 import LocalBarIcon from '@mui/icons-material/LocalBar';
-
-
-
-// const adicionaAoRelatorio = (relatorio, carrinho) => {
-//   const relatorios = Object.values(getLocalStorage('tuca_lanches_relatorios'));
-//   const date = actualData();
-//   const { hour, timeFormat } = actualHour();
-
-//   const vendaAtual = {};
-//   const novoRelatorio = { ...relatorios };
-
-//   //  Organiza os dados da venda atual
-//   Object.values(carrinho).forEach(({
-//     name,
-//     productPrice,
-//     quantity,
-//     costPrice,
-//     category,
-//   }) => {
-//     vendaAtual[name] = {
-//       quantity,
-//       productPrice,
-//       costPrice,
-//       category,
-//     }
-//   });
-
-//   // Verifica se já existe um relatório para a data atual
-//   if (novoRelatorio[date]) {
-//     if (novoRelatorio[date][timeFormat]) {
-//       novoRelatorio[date][timeFormat] = {
-//         ...novoRelatorio[date][timeFormat],
-//         ...vendaAtual,
-//       };
-//       novoRelatorio[date][timeFormat] = vendaAtual;
-//     }
-//     novoRelatorio[date] = {
-//       ...novoRelatorio[date],
-//       [timeFormat]: vendaAtual,
-//     }
-//   } else {
-//     novoRelatorio[date] = {
-//       ...novoRelatorio[date],
-//       [timeFormat]: vendaAtual,
-//     }
-//   }
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import styled from '@emotion/styled';
 
 
 export const VendaDoProduto = () => {
@@ -65,6 +21,8 @@ export const VendaDoProduto = () => {
 
   const { setCarrinhoContext, carrinho } = useCarrinhoContext();
   const { setRelatoriosContext, relatorios } = useRelatoriosContext();
+
+  const theme = useTheme();
 
   useMemo(() => {
     const cartStorage = getLocalStorage('tuca_lanches_cart');
@@ -80,6 +38,15 @@ export const VendaDoProduto = () => {
     setValue(newValue);
   };
 
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      right: -3,
+      top: 13,
+      border: `2px solid ${theme.palette.background.paper}`,
+      padding: '0 4px',
+    },
+  }));
+
   const carrinhoList = Object.values(carrinho);
 
   return (
@@ -87,14 +54,18 @@ export const VendaDoProduto = () => {
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
           {
-            productsType.map((type) => {
+            productsType.map((type, index) => {
               if (type === "Carrinho") {
-                return <Tab icon={<ProductionQuantityLimitsIcon />} label={type} />
+                return <Tab key={index} icon={
+                  <StyledBadge badgeContent={carrinhoList.length} color="default">
+                    <ShoppingCartIcon />
+                  </StyledBadge>
+                } label={type} />
               }
               if (type === "Comida") {
-                return <Tab icon={<LunchDiningIcon />} label={`${type}s`} />
+                return <Tab key={index} icon={<LunchDiningIcon />} label={`${type}s`} />
               }
-              return <Tab icon={<LocalBarIcon />} label={`${type}s`} />
+              return <Tab key={index} icon={<LocalBarIcon />} label={`${type}s`} />
             })
           }
         </Tabs>
@@ -107,6 +78,7 @@ export const VendaDoProduto = () => {
                 {productsType[value] === type && (
                   <>
                     <CardProduct
+                      key={id}
                       id={id}
                       productPrice={productPrice}
                       costPrice={costPrice}
@@ -122,14 +94,24 @@ export const VendaDoProduto = () => {
           })
         }
         {
-          carrinhoList.map((produto) => {
+          (carrinhoList.length === 0 && productsType[value] === "Carrinho") && (
+            <Box>
+              <Typography variant="h5" component="div" sx={{ marginTop: "20px", textAlign:"center", color: theme.palette.primary.dark }}>
+              <ProductionQuantityLimitsIcon /> Sem produtos no carrinho.
+              </Typography>
+            </Box>
+          )
+        }
+        {
+          carrinhoList.map((produto, index) => {
             const { id, productPrice, costPrice, type, name, imgLink, category } = produto;
-            return (carrinhoList.length === 0 ? <Typography>Nenhum produto no carrinho</Typography> :
+            return (
               (
-                <div >
+                <div key={id}>
                   {productsType[value] === "Carrinho" && (
                     <>
                       <CardProduct
+                        key={id}
                         id={id}
                         productPrice={productPrice}
                         costPrice={costPrice}
@@ -147,25 +129,25 @@ export const VendaDoProduto = () => {
         }
       </Box>
 
-       {/* Botão de finalizar compra */}
+      {/* Botão de finalizar compra */}
       {
-        (productsType[value] === "Carrinho" && carrinhoList.length > 0 ) && (
+        (productsType[value] === "Carrinho" && carrinhoList.length > 0) && (
           <Box>
-              <Button
-                onClick={() => { 
-                  const novoRelatório = adicionaAoRelatorio(relatorios, carrinhoList);
-                  setRelatoriosContext(novoRelatório);
-                  setCarrinhoContext({});
-                }}
-                type="button"
-                variant="contained"
-                color="success"
-                fullWidth
-                disabled={false}
-                sx={{ marginTop: "20px" }}
-              >
-                Finalizar
-              </Button>
+            <Button
+              onClick={() => {
+                const novoRelatório = adicionaAoRelatorio(relatorios, carrinhoList);
+                setRelatoriosContext(novoRelatório);
+                setCarrinhoContext({});
+              }}
+              type="button"
+              variant="contained"
+              color="success"
+              fullWidth
+              disabled={false}
+              sx={{ marginTop: "20px" }}
+            >
+              Finalizar
+            </Button>
           </Box>
         )
       }
